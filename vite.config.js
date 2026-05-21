b6e0a78a72eb2cad1187ec396ac1438ed9fd7f89
moollc <135183296+moollc@users.noreply.github.com>
2026-05-21 12:14:55 -0500

    load fix

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,10 @@ import preact from '@preact/preset-vite';
 
 export default defineConfig({
   plugins: [preact()],
-  base: './', 
+  base: '/',
   server: {
     port: 5173,
     strictPort: true,
-    // Headers removed to prevent blocking external fonts/icons
   },
   build: {
     outDir: 'dist',
